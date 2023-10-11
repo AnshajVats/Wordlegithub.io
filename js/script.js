4a97f5d7047076015473a7d7d96fc2b39d3523b8
Anshaj vats <9931633906vats@gmail.com>
2023-10-11 16:28:21 -0700

    added the squares on the board

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+ makes the sidenav open and close when the hamburger icon is clicked
+*/
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+  
---
+++ js/wordle.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    createSquare();
+    function createSquare() {
+        const gameBoard = document.getElementById("board");
+    
+        for (let index = 0; index < 30; index++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.setAttribute("id", index + 1);
+            gameBoard.appendChild(square);
+            
+        }
+    }
+});
